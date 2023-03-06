16f5a4a0f4ce5dc8cc2655bc9422fcc366443c1e
Neway Aragaw <niwaygebrie@gmail.com>
2023-03-06 22:08:35 +0000

    terenary operators

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -14,7 +14,11 @@ public class Lab {
      * @return if b is true, return true. if b is false, return false.
      */
     public boolean returnBoolean(boolean b){
+      if (b==true) {
+        return true;
+      } else {
         return false;
+      }
     }
 
     /**
@@ -23,7 +27,11 @@ public class Lab {
      * look into boolean operations to do this without using if/else statements.
      */
     public boolean returnNot(boolean b){
-        return false;
+        if (b==true) {
+            return false;
+        } else {
+            return true;
+        }
     }
 
     /**
@@ -33,7 +41,8 @@ public class Lab {
      * look into boolean operations to do this without using if/else statements.
      */
     public boolean returnAnd(boolean b1, boolean b2){
-        return false;
+        boolean result=(b1&&b2)==true?true:false;
+        return result;
     }
 
     /**
@@ -43,7 +52,8 @@ public class Lab {
      * look into boolean operations to do this without using if/else statements.
      */
     public boolean returnOr(boolean b1, boolean b2){
-        return false;
+        boolean result=(b1||b2)==true?true:false;
+        return result;
     }
 
 
